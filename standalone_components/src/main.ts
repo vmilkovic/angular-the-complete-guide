20b87be61476dbd47c856c3307dd71a01f1486da
Vedran Milković <vedran.milkovic25@gmail.com>
2023-06-04 18:13:16 +0200

    Standalone Components

---
+++ standalone_components/src/main.ts
@@ -0,0 +1,16 @@
+import { enableProdMode, importProvidersFrom } from "@angular/core";
+import { environment } from "./environments/environment";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+import { AppRoutingModule } from "./app/app-routing.module";
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    // AnalyticsService
+    importProvidersFrom(AppRoutingModule),
+  ],
+});
